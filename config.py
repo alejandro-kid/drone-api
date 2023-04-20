da37029b5f33e264095d55a654e15d22df2aa410
Alejandro Roble Gutierrez <arobleg@gmail.com>
2023-04-20 01:46:06 -0400

    Add environment variable to configure DB

--- config.py
+++ config.py
@@ -5,4 +5,8 @@ import connexion
 
 drone_api = connexion.App(__name__, specification_dir='./swagger')
 
+drone_api.app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DB_HOST",
+                                                "postgresql://postgres:postgres@localhost:5432/drone_api")
+drone_api.app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 drone_api.add_api('swagger.yml')
