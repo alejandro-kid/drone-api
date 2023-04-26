c5e560d063efbaaad11d3b64706921baf5b23966
Alejandro Roble Gutierrez <arobleg@gmail.com>
2023-04-25 20:32:24 -0400

    implement a class for db conection

--- config.py
+++ config.py
@@ -1,12 +1,15 @@
-import os
 import connexion
+
+from DBConfig import DBConfig
 
 # create an application instance
 
 drone_api = connexion.App(__name__, specification_dir='./swagger')
 
-drone_api.app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DB_HOST",
-                                                "postgresql://postgres:postgres@localhost:5432/drone_api")
+db_configuration = DBConfig()
+
+drone_api.app.config['SQLALCHEMY_DATABASE_URI'] = db_configuration.get_uri()
+
 drone_api.app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 drone_api.add_api('swagger.yml')
