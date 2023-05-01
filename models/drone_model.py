9db55ede13f714accd33179e64fb44250a712c17
Alejandro Roble Gutierrez <arobleg@gmail.com>
2023-05-01 04:19:16 -0400

    Create add medicin funtion

--- models/drone_model.py
+++ models/drone_model.py
@@ -1,12 +1,13 @@
 import json
 from db_config import db
+from models.medicine_model import Medication
 
 
 class Drone(db.Model):
     __tablename__ = 'drone'
 
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    serial_number = db.Column(db.String(100), unique=True, nullable=False)
+    serial_number = db.Column(db.String(100), primary_key=True, unique=True, \
+        nullable=False)
     model = db.Column(db.String(20), db.CheckConstraint("model IN ('Lightweight', \
         'Middleweight', 'Cruiserweight', 'Heavyweight')"), nullable=False)
     weight_limit = db.Column(db.Float, nullable=False)
@@ -35,14 +36,16 @@ class Drone(db.Model):
             case _:
                 self.weight_limit = 0
 
-    
-    def register_drone(self):
-        db.session.add(self)
+    @staticmethod
+    def register_drone(serial_number, model, battery_capacity=100, \
+             state='IDLE'):
+        new_drone = Drone(serial_number, model, battery_capacity)
+        db.session.add(new_drone)
         db.session.commit()
+
 
     def __repr__(self):
         location_object = {
-            'id': self.id,
             'serial_number': self.serial_number,
             'model': self.model,
             'weight_limit': str(self.weight_limit) + ' g',
@@ -50,3 +53,46 @@ class Drone(db.Model):
             'state': self.state
         }
         return json.dumps(location_object)
+
+
+    def add_medication(self, medications: list) -> object:
+
+        stored_and_not_stored = {
+            "stored": [],
+            "not_stored": []
+        }
+        commit = False
+
+        if self.state == 'LOADING' or self.state == 'IDLE':
+            loaded_medications = Medication.query.filter_by(
+                drone_id=self.serial_number
+                ).all()
+            weight_space = self.weight_limit - \
+                sum([medication.weight for medication in loaded_medications])
+
+            if weight_space > 0:
+                for medication in medications:
+                    if weight_space >= medication["weight"]:
+                        weight_space -= medication["weight"]
+                        medication = Medication(medication["name"],
+                            medication["weight"], medication["code"],
+                            'LOADED', self.serial_number)
+                        db.session.add(medication)
+                        stored_and_not_stored["stored"].append(medication)
+                        commit = True
+                    else:
+                        stored_and_not_stored["not_stored"].append(medication)
+
+                    if weight_space == 0:
+                        break
+            else:
+                stored_and_not_stored["not_stored"].extend(medications)
+
+            if commit:
+                if self.state == 'IDLE':
+                    Drone.query.filter_by(serial_number=self.serial_number).\
+                        update({'state': 'LOADING'})
+                db.session.commit()
+
+        return stored_and_not_stored
+
--- models/medicine_model.py
+++ models/medicine_model.py
@@ -9,22 +9,24 @@ class Medication(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     weight = db.Column(db.Float, nullable=False)
-    code = db.Column(db.String(20), nullable=False)
-    image = db.Column(db.String(100), nullable=False)
+    code = db.Column(db.String(20), unique=True, nullable=False)
+    status = db.Column(db.String(20), db.CheckConstraint("status IN ('LOADED', \
+        'DELIVERING', 'DELIVERED')"), nullable=False)
+    drone_id = db.Column(db.String(100), db.ForeignKey('drone.serial_number'))
 
-    drone_id = db.Column(db.String(100), db.ForeignKey('drone.id'))
-
-    def __init__(self, name, weight=None, code=None, image=None):
+    def __init__(self, name, weight=None, code=None, status='LOADED', \
+        drone_id=None):
         self.name = name
         self.weight = weight
         self.code = code
-        self.image = image
+        self.status = status
+        self.drone_id = drone_id
 
     def __repr__(self):
         location_object = {
             'name': self.name,
             'weight': self.weight,
             'code': self.code,
-            'image': self.image
+            'status': self.status
         }
         return json.dumps(location_object)
