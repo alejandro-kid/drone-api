2adb0d608d1d1e365238e96c4ea34e9745979d77
Alejandro Roble Gutierrez <arobleg@gmail.com>
2023-05-08 02:29:01 -0400

    Add regsiter an existed drone test

--- api/drone.py
+++ api/drone.py
@@ -9,13 +9,37 @@ from sqlalchemy import exc
 def register_drone():
     request_data = request.get_json()
     try:
-        jsonschema.validate(request_data, register_drone_schema)
-        new_drone = Drone(request_data['serial_number'], request_data['model'])
-        new_drone.register_drone()
-        response = Response(json.dumps(str(new_drone)), 201, \
-            mimetype="application/json")
+        if not \
+            Drone.query.filter_by(serial_number=request_data['serial_number']).first():
+
+            jsonschema.validate(request_data, register_drone_schema)
+            new_drone = Drone(request_data['serial_number'], request_data['model'])
+            new_drone.register_drone()
+            data = {
+                "sucess": True,
+                "message": "Drone registered successfully",
+                "data": {
+                    "serial_number": new_drone.serial_number,
+                    "model": new_drone.model,
+                    "battery": new_drone.battery_capacity,
+                    "weight_limit": new_drone.weight_limit
+                }
+            }
+            response = Response(json.dumps(data), 201, \
+                mimetype="application/json")
+        else:
+            data = {
+                "sucess": False,
+                "message": "Drone already registered"
+            }
+            response = Response(json.dumps(data), 202, mimetype="application/json")
+
     except jsonschema.exceptions.ValidationError as exc:
         response = Response(str(exc.message), 400, mimetype="application/json")
+
+    except exc.SQLAlchemyError as error:
+        response = Response(str(error), 400, mimetype="application/json")
+
     return response
 
 
--- tests/test_service/test_drone.py
+++ tests/test_service/test_drone.py
@@ -18,18 +18,36 @@ def test_register_drone(client):
     td_model = "Lightweight"
 
     response = client.post("/register_drone", data=json.dumps(dict(
-        serial_number="1234567",
-        model="Lightweight"
+        serial_number=td_serial_number,
+        model=td_model
     )), mimetype='application/json')
 
     json_info = helper(response.response)
 
     assert response.status_code == 201
     assert response.content_type == 'application/json'
+    assert json_info["sucess"] is True
+    assert json_info["message"] == "Drone registered successfully"
     assert json_info["data"]["serial_number"] == td_serial_number
     assert json_info["data"]["model"] == td_model
     assert json_info["data"]["weight_limit"] == 200.0
     assert json_info["data"]["battery"] == 100
 
 
+def test_register_existed_drone(client):
+
+    td_serial_number = "HTU8L8SO2Xx3EYmkKAz4iuFBY2gAo"
+    td_model = "Cruiserweight"
+
+    response = client.post("/register_drone", data=json.dumps(dict(
+        serial_number=td_serial_number,
+        model=td_model
+    )), mimetype='application/json')
+
+    json_info = helper(response.response)
+
+    assert response.status_code == 202
+    assert response.content_type == 'application/json'
+    assert json_info["sucess"] is False
+    assert json_info["message"] == "Drone already registered"
 
