1517afed12a6e70b47b223c531e70c6b7cc942e1
Alejandro Roble Gutierrez <arobleg@gmail.com>
2023-05-10 02:24:38 -0400

    Implement get and put drone

--- api/drone.py
+++ api/drone.py
@@ -1,8 +1,9 @@
 import jsonschema
 
 from flask import Response, request, json
-from schemas.drone_schema import register_drone_schema, add_medicine_schema
+from schemas.drone_schema import register_drone_schema, add_medicine_schema, update_drone_schema
 from models.drone_model import Drone
+from models.medicine_model import Medication
 from sqlalchemy import exc
 
 
@@ -16,7 +17,7 @@ def register_drone():
             new_drone = Drone(request_data['serial_number'], request_data['model'])
             new_drone.register_drone()
             data = {
-                "sucess": True,
+                "success": True,
                 "message": "Drone registered successfully",
                 "data": {
                     "serial_number": new_drone.serial_number,
@@ -29,7 +30,7 @@ def register_drone():
                 mimetype="application/json")
         else:
             data = {
-                "sucess": False,
+                "success": False,
                 "message": "Drone already registered"
             }
             response = Response(json.dumps(data), 202, mimetype="application/json")
@@ -50,7 +51,8 @@ def load_drone():
         serial_number = request_data['serial_number']
         stored_drone = Drone.query.filter_by(serial_number=serial_number).first()
         if stored_drone.battery_capacity >= 25:
-            stored_and_not_stored = stored_drone.add_medication(request_data['medications'])
+            stored_and_not_stored = \
+                stored_drone.add_medication(request_data['medications'])
             data = {
                 "success": True,
                 "data": {
@@ -74,6 +76,140 @@ def load_drone():
             }
             response = Response(json.dumps(data), 202, \
                 mimetype="application/json")
+
+    except jsonschema.exceptions.ValidationError as exc:
+        response = Response(str(exc.message), 400, mimetype="application/json")
+    return response
+
+
+def get_drone(serial_number):
+    try:
+        stored_drone = Drone.query.filter_by(serial_number=serial_number).first()
+        if stored_drone:
+            medicines = \
+                Medication.query.filter_by(drone_id=stored_drone.serial_number).all()
+            data = {
+                "success": True,
+                "data": {
+                    "drone": {
+                        "serial_number": stored_drone.serial_number,
+                        "model": stored_drone.model,
+                        "battery": "{}%".format(stored_drone.battery_capacity),
+                        "weight_limit": "{}g".format(stored_drone.weight_limit),
+                        "state": stored_drone.state
+                    },
+                    "medicines": [{
+                        "name": medicine.name,
+                        "weight": "{}g".format(medicine.weight),
+                        "code": medicine.code,
+                        "state": medicine.state
+                    } for medicine in medicines]
+                }
+            }
+            response = Response(json.dumps(data), 200, mimetype="application/json")
+        else:
+            data = {
+                "success": False,
+                "message": "Drone not found"
+            }
+            response = Response(json.dumps(data), 404, mimetype="application/json")
+
+    except jsonschema.exceptions.ValidationError as exc:
+        response = Response(str(exc.message), 400, mimetype="application/json")
+    return response
+
+
+def update_drone(serial_number):
+    request_data = request.get_json()
+    try:
+        jsonschema.validate(request_data, update_drone_schema)
+        stored_drone = Drone.query.filter_by(serial_number=serial_number).first()
+        if stored_drone:
+            if request_data['state']:
+                match request_data['state']:
+                    case 'IDLE':
+                        if stored_drone.state == 'RETURNING':
+                            stored_drone.update_state('IDLE')
+                            success = True
+                            message = "Drone state changed successfully"
+                        else:
+                            success = False
+                            message = "You only change from RETURNING to IDLE state"
+
+                    case 'LOADING':
+                            success = False
+                            message = \
+                                "The only way to change to LOADING is adding medications"
+
+                    case 'LOADED':
+                            if stored_drone.state == 'LOADING':
+                                stored_drone.update_state('LOADED')
+                                success = True
+                                message = "Drone state changed successfully"
+                            else:
+                                success = False
+                                message = "You only change from LOADING to LOADED state"
+
+                    case 'DELIVERING':
+                            if stored_drone.state in ['LOADED', 'LOADING']:
+                                stored_drone.update_state(state='DELIVERING')
+                                success = True
+                                message = "Drone state changed successfully"
+                            else:
+                                success = False
+                                message = \
+                                    "You only change from LOADED or LOADING" \
+                                    " to DELIVERING state"
+
+                    case "DELIVERED":
+                            if stored_drone.state == 'DELIVERING':
+                                stored_drone.update_state('DELIVERED')
+                                Medication.update_all_state(\
+                                        stored_drone.serial_number, "DELIVERED"\
+                                )
+                                success = True
+                                message = "Drone state changed successfully"
+                            else:
+                                success = False
+                                message = \
+                                    "You only change from DELIVERING to DELIVERED state"
+
+                    case "RETURNING":
+                            if stored_drone.state in ['DELIVERED', 'DELIVERING']:
+                                if stored_drone.state == 'DELIVERING':
+                                    stored_drone.update_state('DELIVERING')
+                                    Medication.update_all_state(\
+                                        stored_drone.serial_number, "LOADED"\
+                                    )
+                                    success = True
+                                    message = "Drone state changed successfully"
+                            else:
+                                success = False
+                                message = \
+                                    "You only change from DELIVERED or DELIVERING" \
+                                    " to RETURNING state"
+                    case _:
+                        success = False
+                        message = "Invalid state"
+
+                data = {
+                    "success": success,
+                    "message": message
+                }
+                response = Response(json.dumps(data), 200, mimetype="application/json")
+            if request_data['battery']:
+                stored_drone.update_battery(request_data['battery_capacity'])
+                data = {
+                    "success": True,
+                    "message": "Drone battery changed successfully"
+                }
+                response = Response(json.dumps(data), 200, mimetype="application/json")
+        else:
+            data = {
+                "success": False,
+                "message": "Drone not found"
+            }
+            response = Response(json.dumps(data), 404, mimetype="application/json")
 
     except jsonschema.exceptions.ValidationError as exc:
         response = Response(str(exc.message), 400, mimetype="application/json")
